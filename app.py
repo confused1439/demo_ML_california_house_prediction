fa549a7c4220e0b7a79178a6e7b789f027c3ebd3
confused1439 <saradvavivek@gmail.com>
2024-07-09 18:12:41 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import pickle, numpy as np, pandas as pd 
+from flask import Flask, request, app, jsonify, url_for, render_template
+
+app= Flask(__name__)
+model=pickle.load(open("linear_reg_model.pkl", 'rb'))
+scaler=pickle.load(open("scaler.pkl", 'rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+    output=model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    output = model.predict(scaler.transform(np.array(data).reshape(1, -1)))[0]
+    return render_template("index.html", prediction_test="The House price prediction is {}".format(output))
+
+if __name__ == '__main__':
+    app.run(debug=True)
